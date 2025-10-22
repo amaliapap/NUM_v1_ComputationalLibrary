dc72f3a5927c4d7895038ac2eee8011fe0fbb089
kenhasteandersen <kha@aqua.dtu.dk>
2025-10-22 15:18:31 +0200

    fixed p=0.5

--- matlab/input/input.h
+++ matlab/input/input.h
@@ -190,7 +190,7 @@
 
 ! Vulnerability to predation:
 !----------------------------
-  palatability = 0.75
+  palatability = 0.5
   
 !***********************************************************************
 ! COPEPODS ACTIVE INPUT PARAMETERS
